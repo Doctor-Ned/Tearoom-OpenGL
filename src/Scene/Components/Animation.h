4b56a70518349f478bd0ad04735bda675a19f3aa
Michal Mamona <michalha1174@gmail.com>
2019-04-26 11:36:44 +0200

    Preparing animation to collect data about different objects animation.

--- src/Scene/Components/Animation.h
+++ src/Scene/Components/Animation.h
@@ -2,23 +2,29 @@
 #define ANIMATION_H
 #include "Scene/Components/Component.h"
 #include <map>
-
+struct ObjectAnimation
+{
+	std::map<float, glm::vec3> translation;
+	std::map<float, glm::vec3> scale;
+	std::map<float, glm::vec3> rotation;
+};
 class Animation : public Component
 {
 private:
+	std::string name = "animation";
 	float speed = 1.0f;
 	float currentTime = 0.0f;
 	float endTime = 0.0f;
 	bool isPlaying = false;
-	glm::vec3 rotVec;
-	std::map<float, glm::vec3> translation;
-	std::map<float, glm::vec3> scale;
-	std::map<float, glm::vec3> rotation;
-	void translationInterpolation(float currentTime);
-	void scaleInterpolation(float currentTime);
-	void rotationInterpolation(float currentTime);
+	
+	void translationInterpolation(float currentTime, GraphNode* animatedObject, std::map<float, glm::vec3>& translation);
+	void scaleInterpolation(float currentTime, GraphNode* animatedObject, std::map<float, glm::vec3>& scale);
+	void rotationInterpolation(float currentTime, GraphNode* animatedObject, std::map<float, glm::vec3>& rotation);
+
+	std::map<GraphNode*, ObjectAnimation> objectAnimations;
+	void setEndTime();
 public:
-	Animation(GraphNode* gameObject);
+	Animation(GraphNode* gameObject, std::string&& name);
 	Animation(){}
 	SerializableType getSerializableType() override;
 	Json::Value serialize(Serializer* serializer) override;
