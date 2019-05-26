766806e6a4fd206adf6edf34e17031804509ab33
Michał Białecki <miszekb@gmail.com>
2019-05-26 12:39:33 +0200

    added "About" window

--- src/Scene/Scenes/MenuPreview.cpp
+++ src/Scene/Scenes/MenuPreview.cpp
@@ -6,26 +6,55 @@
 #include "MenuPreview.h"
 #include <Ui/UiColorPlane.h>
 #include "MenuScene.h"
+#include "MiszukScene.h"
 
 MenuPreview::MenuPreview() {
     UiPlane*  menuBackground = new UiPlane("res/textures/menuSideBar.PNG", glm::vec2(-1.0f, -10.0f), glm::vec2(380.0f, 800.0f), TopLeft);
+    UiPlane*  menuAbout = new UiPlane("res/textures/aboutMenu.PNG", glm::vec2(-1.0f, -10.0f), glm::vec2(380.0f, 800.0f), TopLeft);
     UiButton* startGame = new UiButton(glm::vec2(98.0f, 210.0f), glm::vec2(170.0f,80.0f), Left);
     UiButton* options = new UiButton(glm::vec2(98.0f, 310.0f), glm::vec2(170.0f,80.0f), Left);
     UiButton* about = new UiButton(glm::vec2(98.0f, 410.0f), glm::vec2(170.0f,80.0f), Left);
     UiButton* quit = new UiButton(glm::vec2(98.0f, 500.0f), glm::vec2(170.0f,80.0f), Left);
 
+    UiButton* backToMenu = new UiButton(glm::vec2(101.0f, 675.0f), glm::vec2(170.0f,80.0f), Left);
+
+
     options->setOpacity(0.0f);
     startGame->setOpacity(0.0f);
     about->setOpacity(0.0f);
     quit->setOpacity(0.0f);
+    backToMenu->setOpacity(0.0f);
 
     UiColorPlane* startHover = new UiColorPlane(glm::vec4(0.0f, 0.0f, 0.0f, 0.8f), glm::vec2(98.0f, 210.0f), glm::vec2(170.0f,80.0f), Left);
     UiColorPlane* optionsHover = new UiColorPlane(glm::vec4(0.0f, 0.0f, 0.0f, 0.8f), glm::vec2(98.0f, 310.0f), glm::vec2(170.0f,80.0f), Left);
     UiColorPlane* aboutHover = new UiColorPlane(glm::vec4(0.0f, 0.0f, 0.0f, 0.8f), glm::vec2(98.0f, 405.0f), glm::vec2(170.0f,80.0f), Left);
-    UiColorPlane* quitHover = new UiColorPlane(glm::vec4(0.0f, 0.0f, 0.0f, 0.8f), glm::vec2(98.0f, 503.0f), glm::vec2(170.0f,80.0f), Left);
+    UiColorPlane* quitHover = new UiColorPlane(glm::vec4(0.0f, 0.0f, 0.0f, 0.8f), glm::vec2(100.0f, 503.0f), glm::vec2(170.0f,80.0f), Left);
+    UiColorPlane* backHover = new UiColorPlane(glm::vec4(0.0f, 0.0f, 0.0f, 0.8f), glm::vec2(100.0f, 678.0f), glm::vec2(170.0f,80.0f), Left);
 
+    backToMenu->addHoverCallback([this, backHover]() {
+        backHover->setOpacity(0.3f);c
+    });
+    backToMenu->addLeaveCallback([this, backHover]() {
+        backHover->setOpacity(0.0f);
+    });
+    backToMenu->addClickCallback([this, menuAbout, menuBackground, startGame, startHover,
+                                         options, optionsHover, quit, quitHover, about, aboutHover, backToMenu, backHover]() {
+        menuBackground->setActive(true);
+        menuAbout->setActive(false);
+        startGame->setActive(true);
+        startHover->setActive(true);
+        options->setActive(true);
+        optionsHover->setActive(true);
+        quit->setActive(true);
+        quitHover->setActive(true);
+        about->setActive(true);
+        aboutHover->setActive(true);
+
+        backToMenu->setActive(false);
+        backHover->setActive(false);
+    });
     startGame->addClickCallback([this]() {
-        GameManager::getInstance()->setCurrentScene(new MenuScene());
+        GameManager::getInstance()->setCurrentScene(new MiszukScene());
     });
     startGame->addHoverCallback([this, startHover](){
         startHover->setOpacity(0.3f);
@@ -41,6 +70,22 @@ MenuPreview::MenuPreview() {
         optionsHover->setOpacity(0.0f);
     });
 
+    about->addClickCallback([this, menuAbout, menuBackground, startGame, startHover,
+                             options, optionsHover, quit, quitHover, about, aboutHover, backToMenu, backHover]() {
+       menuBackground->setActive(false);
+       menuAbout->setActive(true);
+       startGame->setActive(false);
+       startHover->setActive(false);
+       options->setActive(false);
+       optionsHover->setActive(false);
+       quit->setActive(false);
+       quitHover->setActive(false);
+       about->setActive(false);
+       aboutHover->setActive(false);
+
+       backToMenu->setActive(true);
+       backHover->setActive(true);
+    });
     about->addHoverCallback([this, aboutHover](){
         aboutHover->setOpacity(0.3f);
     });
@@ -49,7 +94,8 @@ MenuPreview::MenuPreview() {
     });
 
     quit->addClickCallback([this]() {
-        GameManager::getInstance()->quit();
+        //GameManager::getInstance()->quit();
+        GameManager::getInstance()->setCurrentScene(new MenuScene()); //temporary
     });
     quit->addHoverCallback([this, quitHover](){
         quitHover->setOpacity(0.3f);
@@ -64,6 +110,10 @@ MenuPreview::MenuPreview() {
     rootUiElement->addChild(about);
     rootUiElement->addChild(quit);
 
+    rootUiElement->addChild(menuAbout);
+    rootUiElement->addChild(backToMenu);
+    rootUiElement->addChild(backHover);
+
     rootUiElement->addChild(startHover);
     rootUiElement->addChild(optionsHover);
     rootUiElement->addChild(aboutHover);
@@ -73,6 +123,9 @@ MenuPreview::MenuPreview() {
     optionsHover->setOpacity(0.0f);
     aboutHover->setOpacity(0.0f);
     quitHover->setOpacity(0.0f);
+    backHover->setOpacity(0.0f);
+    menuAbout->setActive(false);
+    backToMenu->setActive(false);
 }
 
 MenuPreview::~MenuPreview() {}
