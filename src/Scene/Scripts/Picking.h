23c5b4e3040036e4d2b9d06bf3e57704e16ad3da
Michał Białecki <miszekb@gmail.com>
2019-06-03 22:36:18 +0200

    conflict resolve 2

--- src/Scene/Scripts/Picking.h
+++ src/Scene/Scripts/Picking.h
@@ -12,25 +12,26 @@
 class Camera;
 class Picking : public Component {
 public:
-    Picking(GraphNode* _gameObject, Camera* camera, Scene* scene, const std::string& name = "Picking");
-    Picking(GraphNode* _gameObject, const std::string& name = "Picking");
-    void update(float msec) override;
-    ~Picking() override;
-    SerializableType getSerializableType() override;
-    Json::Value serialize(Serializer *serializer) override;
-    void deserialize(Json::Value &root, Serializer* serializer) override;
-    void showInventoryUi();
-    void hideInventoryUi();
-    void hidePreview();
-    void showPreview();
-    bool getSwitch();
-    void setSwitch(bool ifShown);
-    Scene *getScene() const;
-    UiCanvas *getInventoryCanvas() const;
-    void setCamera(Camera *camera);
-    Camera* getCamera();
-    void placeInGrid(ItemType itype, UiCanvas* canvas);
+	Picking(GraphNode* _gameObject, Camera* camera, Scene* scene, const std::string& name = "Picking");
+	Picking(GraphNode* _gameObject, const std::string& name = "Picking");
+	void update(float msec) override;
+	~Picking() override;
+	SerializableType getSerializableType() override;
+	Json::Value serialize(Serializer *serializer) override;
+	void deserialize(Json::Value &root, Serializer* serializer) override;
+	void showInventoryUi();
+	void hideInventoryUi();
+	void hidePreview();
+	void showPreview();
+	bool getSwitch();
+	void setSwitch(bool ifShown);
+	Scene *getScene() const;
+	UiCanvas *getInventoryCanvas() const;
+	void setCamera(Camera *camera);
+	Camera* getCamera();
+	void placeInGrid(ItemType itype);
 private:
+<<<<<<< HEAD
     void renderGui() override;
     void initialize();
     void collect(CollectableObject *collectable);
@@ -53,6 +54,28 @@ private:
     UiPlane* letterInventory;
     UiColorPlane* descBackground;
     GraphNode* currentInteraction = nullptr;
+=======
+	void initialize();
+	void collect(CollectableObject *collectable);
+	Scene* scene;
+	float distance = 1.0f;
+	bool inventoryUI = false;
+	UiCanvas *inventoryCanvas, *encouragementCanvas, *previewCanvas;
+	Camera* camera;
+	std::vector<GraphNode*> inventory;
+	Picking() {}
+	friend class Serializer;
+	UiColorPlane* encouragementBackground;
+	UiText *encouragementPick, *encouragementActivate;
+	UiButton* itemsButton;
+	UiButton* letterButton;
+	UiButton* photoButton;
+	UiPlane* photosInventory;
+	UiPlane* itemsInventory;
+	UiPlane* letterInventory;
+	UiColorPlane* descBackground;
+	GraphNode* currentInteraction = nullptr;
+>>>>>>> parent of 1abcab9... Picking class refactor
 };
 
 #endif
