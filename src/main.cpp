11118281ff62c0721595d432ec2b3525122fa2f2
MickAlmighty <michalha1174@gmail.com>
2019-07-05 13:33:54 +0200

    Addition debug callback (not integrated with logger yet -> only console output)

--- src/main.cpp
+++ src/main.cpp
@@ -27,15 +27,33 @@
 #include <filesystem>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include <chrono>
+#include <thread>
 
 //comment extern below if you don't have NVidia GPU
-extern "C" {
-	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-}
+//extern "C" {
+//	_declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+//}
 
 static void glfw_error_callback(int error, const char* description) {
 	SPDLOG_ERROR("GLFW error {}: {}", error, description);
 }
+
+#define OPENGL_DEBUG_MODE
+
+#ifdef OPENGL_DEBUG_MODE
+	#define ACTIVATE_OPENGL_DEBUG_MODE() glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#else
+	#define ACTIVATE_OPENGL_DEBUG_MODE()
+#endif
+
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar *message,
+	const void *userParam);
 
 static GameManager* gameManager;
 static AssetManager* assetManager;
@@ -51,7 +69,8 @@ void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, in
 		if (action == GLFW_PRESS && !uiButtonPressed) {
 			uiButtonPressed = true;
 			showUi = !showUi;
-		} else if (action == GLFW_RELEASE && uiButtonPressed) {
+		}
+		else if (action == GLFW_RELEASE && uiButtonPressed) {
 			uiButtonPressed = false;
 		}
 	}
@@ -59,7 +78,8 @@ void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, in
 		if (action == GLFW_PRESS && !ssPressed) {
 			ssPressed = true;
 			takeScreenshot = true;
-		} else if (action == GLFW_RELEASE && ssPressed) {
+		}
+		else if (action == GLFW_RELEASE && ssPressed) {
 			ssPressed = false;
 		}
 	}
@@ -67,7 +87,8 @@ void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, in
 		if (action == GLFW_PRESS && !ssPressed) {
 			ssPressed = true;
 			takeUiScreenshot = true;
-		} else if (action == GLFW_RELEASE && ssPressed) {
+		}
+		else if (action == GLFW_RELEASE && ssPressed) {
 			ssPressed = false;
 		}
 	}
@@ -75,7 +96,8 @@ void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, in
 		if (action == GLFW_PRESS && !fpsPressed) {
 			fpsPressed = true;
 			showFps = !showFps;
-		} else if (action == GLFW_RELEASE && fpsPressed) {
+		}
+		else if (action == GLFW_RELEASE && fpsPressed) {
 			fpsPressed = false;
 		}
 	}
@@ -83,7 +105,8 @@ void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, in
 		if (action == GLFW_PRESS && !keysPressed) {
 			keysPressed = true;
 			showKeys = !showKeys;
-		} else if (action == GLFW_RELEASE && keysPressed) {
+		}
+		else if (action == GLFW_RELEASE && keysPressed) {
 			keysPressed = false;
 		}
 	}
@@ -120,7 +143,8 @@ int main(int argc, char** argv) {
 		spdlog::set_default_logger(std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list({ console_sink, file_sink })));
 		spdlog::set_level(spdlog::level::trace);
 
-	} catch (const spdlog::spdlog_ex& ex) {
+	}
+	catch (const spdlog::spdlog_ex& ex) {
 		printf("Failed to initialize the logger! %s\n", ex.what());
 		return 1;
 	}
@@ -130,6 +154,7 @@ int main(int argc, char** argv) {
 	gameManager = GameManager::getInstance();
 	assetManager = AssetManager::getInstance();
 	serializer = Serializer::getInstance();
+
 
 	VideoSettings videoSettings = OptionsScene::loadVideoSettings();
 
@@ -160,24 +185,31 @@ int main(int argc, char** argv) {
 					videoSettings.windowWidth = target;
 					windowSizeDefined = true;
 					expectedWidth = false;
-				} else {
+				}
+				else {
 					videoSettings.windowHeight = target;
 					windowSizeDefined = true;
 					expectedHeight = false;
 				}
 			}
-		} else {
+		}
+		else {
 			if (strcmp("-fullscreen", arg) == 0) {
 				videoSettings.windowType = Fullscreen;
-			} else if (strcmp("-windowed", arg) == 0) {
+			}
+			else if (strcmp("-windowed", arg) == 0) {
 				videoSettings.windowType = Windowed;
-			} else if (strcmp("-borderless", arg) == 0) {
+			}
+			else if (strcmp("-borderless", arg) == 0) {
 				videoSettings.windowType = WindowedBorderless;
-			} else if (strcmp("-width", arg) == 0) {
+			}
+			else if (strcmp("-width", arg) == 0) {
 				expectedWidth = true;
-			} else if (strcmp("-height", arg) == 0) {
+			}
+			else if (strcmp("-height", arg) == 0) {
 				expectedHeight = true;
-			} else if (strcmp("-fs_borderless", arg) == 0) {
+			}
+			else if (strcmp("-fs_borderless", arg) == 0) {
 				videoSettings.windowType = FullscreenBorderless;
 			}
 		}
@@ -185,13 +217,13 @@ int main(int argc, char** argv) {
 
 	OptionsScene::setVideoSettings(videoSettings);
 
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
 	// Setup window
 	glfwSetErrorCallback(glfw_error_callback);
 	if (!glfwInit())
 		return 1;
-
-
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
 	// Decide GL+GLSL versions
 #if __APPLE__
@@ -208,6 +240,7 @@ int main(int argc, char** argv) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 3.0+ only
+	ACTIVATE_OPENGL_DEBUG_MODE();
 #endif
 
 	float screenWidth = videoSettings.windowWidth, screenHeight = videoSettings.windowHeight;
@@ -223,39 +256,40 @@ int main(int argc, char** argv) {
 		}
 	}
 
+
 	gameManager->updateWindowSize(videoSettings.windowWidth, videoSettings.windowHeight, screenWidth, screenHeight, false);
 
 	// Create window with graphics context
 	GLFWwindow* window;
 	switch (videoSettings.windowType) {
-		case ExclusiveFullscreen:
-		{
-			GLFWmonitor *monitor = glfwGetPrimaryMonitor();
-			const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-			glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-			glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-			glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-			glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
-			window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", monitor, nullptr);
-		}
+	case ExclusiveFullscreen:
+	{
+		GLFWmonitor *monitor = glfwGetPrimaryMonitor();
+		const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+		glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+		glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+		glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+		glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+		window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", monitor, nullptr);
+	}
+	break;
+	case FullscreenBorderless:
+		glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+		window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", nullptr, nullptr);
 		break;
-		case FullscreenBorderless:
-			glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-			window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", nullptr, nullptr);
-			break;
-		case Fullscreen:
-			window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", glfwGetPrimaryMonitor(), nullptr);
-			break;
-		case WindowedBorderless:
-			glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-			window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", nullptr, nullptr);
-			break;
-		case Windowed:
-			window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", nullptr, nullptr);
-			break;
-		default:
-			SPDLOG_ERROR("Unknown window type provided!");
-			throw std::exception("Unknown window type provided.");
+	case Fullscreen:
+		window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", glfwGetPrimaryMonitor(), nullptr);
+		break;
+	case WindowedBorderless:
+		glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+		window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", nullptr, nullptr);
+		break;
+	case Windowed:
+		window = glfwCreateWindow(screenWidth, screenHeight, "Tearoom", nullptr, nullptr);
+		break;
+	default:
+		SPDLOG_ERROR("Unknown window type provided!");
+		throw std::exception("Unknown window type provided.");
 	}
 	if (window == nullptr) {
 		return 1;
@@ -278,7 +312,14 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	CHECK_GL_ERROR();
-
+	GLint flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+	{
+		glEnable(GL_DEBUG_OUTPUT);
+		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // makes sure errors are displayed synchronously
+		glDebugMessageCallback(glDebugOutput, nullptr);
+		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+	}
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -369,9 +410,17 @@ int main(int argc, char** argv) {
 	UiPlane *keysPlane = nullptr;
 
 	Shader* fpsPlaneShader = assetManager->getShader(fpsPlane->getShaderType()), *fpsTextShader = assetManager->getShader(fpsText->getShaderType()), *screenTextShader = assetManager->getShader(STScreenTexture),
-	*uiTexShader = assetManager->getShader(STUiTexture);
+		*uiTexShader = assetManager->getShader(STUiTexture);
 
 	SPDLOG_DEBUG("Starting the game loop!");
+
+	std::thread secondThread([]() {
+		while (!assetManager->isLoaded()) {
+			std::cout << "second thread working" << std::endl;
+			using namespace std::chrono_literals;
+			//std::this_thread::sleep_for(8ms);
+		}
+	});
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -475,7 +524,7 @@ int main(int argc, char** argv) {
 				fpsText->render(fpsTextShader);
 			}
 			CHECK_GL_ERROR();
-			if(showKeys && keysPlane != nullptr) {
+			if (showKeys && keysPlane != nullptr) {
 				uiTexShader->use();
 				keysPlane->render(uiTexShader);
 			}
@@ -489,7 +538,8 @@ int main(int argc, char** argv) {
 		if (takeScreenshot || takeUiScreenshot) {
 			if (takeUiScreenshot) {
 				SPDLOG_DEBUG("Creating a new screenshot without UI...");
-			} else {
+			}
+			else {
 				SPDLOG_DEBUG("Creating a new standard screenshot...");
 			}
 			int totalSize = 3 * videoSettings.windowWidth * videoSettings.windowHeight;
@@ -530,6 +580,7 @@ int main(int argc, char** argv) {
 		}
 	}
 
+	secondThread.join();
 	SPDLOG_DEBUG("Game loop closed! Shutting down...");
 
 	delete gameManager;
@@ -541,4 +592,51 @@ int main(int argc, char** argv) {
 	glfwTerminate();
 
 	return 0;
+}
+
+void APIENTRY glDebugOutput(GLenum source,
+	GLenum type,
+	GLuint id,
+	GLenum severity,
+	GLsizei length,
+	const GLchar *message,
+	const void *userParam)
+{
+	// ignore non-significant error/warning codes
+	if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+
+	std::cout << "---------------" << std::endl;
+	std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+	switch (source)
+	{
+	case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+	case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+	case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+	case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+	case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+	} std::cout << std::endl;
+
+	switch (type)
+	{
+	case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+	case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+	case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+	case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+	case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+	case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+	case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+	} std::cout << std::endl;
+
+	switch (severity)
+	{
+	case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+	case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+	case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+	case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+	} std::cout << std::endl;
+	std::cout << std::endl;
 }
